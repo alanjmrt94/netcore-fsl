3eb2a931dc4a7f0a88f935817f03a6be02bef933
alanjmrt94 <alanjmartinez94@gmail.com>
2021-12-28 04:30:46 -0300

    Update TEST

--- NetcoreTEST/Program.cs
+++ NetcoreTEST/Program.cs
@@ -1,6 +1,15 @@
 ﻿using NetcoreFSL;
+using NetcoreFSL.Searcher;
+using NetcoreFSL.Searcher.Enums;
 
-var fsl = new Fsl();
+Fsl fsl = new();
 fsl.Name = "Alan";
-
 Console.WriteLine(fsl.TestGreeting);
+
+// Test on Linux:
+string folder = "/etc";
+string pattern = ".conf";
+FileSearch fileSearch = new(ExecuteHandlers.InCurrentTask, folder, pattern);
+Console.WriteLine("Running TEST...");
+fileSearch.StartSearch();
+Console.WriteLine("TEST finished.");
